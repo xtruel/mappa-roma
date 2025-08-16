2f782fac2ce145dbe9f0df9f8ce0a3877a83e300
xtruel <truel3000lofi@gmail.com>
2025-08-16 21:58:34 +0200

    Create luoghi.js

---
+++ luoghi.js
@@ -0,0 +1,31 @@
+// luoghi.js
+// Qui definiamo SOLO l’array dei luoghi
+// Accessibile da index.html come window.PLACES
+
+window.PLACES = [
+  {
+    name: "Colosseo",
+    desc: "Icona di Roma, simbolo dell’Impero",
+    coords: [12.4924, 41.8902]
+  },
+  {
+    name: "Foro Romano",
+    desc: "Centro politico e culturale dell’antica Roma",
+    coords: [12.4853, 41.8925]
+  },
+  {
+    name: "Pantheon",
+    desc: "Tempio di tutti gli dei, con la cupola spettacolare",
+    coords: [12.4768, 41.8986]
+  },
+  {
+    name: "Trastevere",
+    desc: "Quartiere storico e pittoresco di Roma",
+    coords: [12.4663, 41.8896]
+  },
+  {
+    name: "San Pietro",
+    desc: "La basilica più famosa del mondo",
+    coords: [12.4534, 41.9022]
+  }
+];
