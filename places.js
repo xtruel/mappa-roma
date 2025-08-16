1b77c30a8f8557f82d422542b1813b8646e06551
xtruel <truel3000lofi@gmail.com>
2025-08-16 19:54:19 +0200

    Add files via upload

---
+++ places.js
@@ -0,0 +1,10 @@
+const PLACES = [
+  {name: "Colosseo", coords: [41.8902, 12.4922], category: "Monumenti", color: "#1e90ff", icon: "🏛️"},
+  {name: "Pantheon", coords: [41.8986, 12.4769], category: "Monumenti", color: "#1e90ff", icon: "🏛️"},
+  {name: "Piazza Navona", coords: [41.8992, 12.4731], category: "Piazze", color: "#ffd700", icon: "🟨"},
+  {name: "Fontana di Trevi", coords: [41.9009, 12.4833], category: "Piazze", color: "#ffd700", icon: "🟨"},
+  {name: "San Pietro", coords: [41.9022, 12.4534], category: "Chiese", color: "#800080", icon: "✝️"},
+  {name: "Catacombe di San Sebastiano", coords: [41.8571, 12.5119], category: "Underground", color: "#000000", icon: "🎶"},
+  {name: "Shari Vari Playhouse", coords: [41.8959, 12.4776], category: "Nightlife", color: "#dc143c", icon: "🍻"},
+  {name: "Villa Borghese", coords: [41.9142, 12.4923], category: "Parchi", color: "#228b22", icon: "🌳"}
+];
